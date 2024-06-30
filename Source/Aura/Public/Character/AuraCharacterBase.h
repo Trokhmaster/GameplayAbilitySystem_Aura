ef5ce85bc4a9bb7c6441677895df5896bd6385e4
Dmitriy <dmitrokh2009@gmail.com>
2024-06-30 18:04:40 +0400

    Character Blueprint setup

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
 
-UCLASS()
+UCLASS(Abstract)
 class AURA_API AAuraCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
@@ -18,6 +18,9 @@ public:
 protected:
 
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere,Category="Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 
 
 };
