22566b0d9fda090fe0e7550247c32ec35bc1b5e6
Dmitriy <dmitrokh2009@gmail.com>
2024-07-02 03:11:49 +0400

    Highlight Enemies

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,66 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility,false,CursorHit);
+	if(!CursorHit.bBlockingHit) return;
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+	/* 
+	 * A. LasActor is null and ThisActor is null
+	 *		do nothing
+	 * B. LastActor ==null , this actor!=null
+	 *		HighLight ThisActor
+	 *	C. LastActor&ThisActor!=null and LastActor!=ThisActor
+	 *		HighLight ThisActor. UnHighLight LastActor
+	 *	D. LastActor&ThisActor!=null and LastActor==ThisActor
+	 *		do nothing
+	 *	E. LastActor != null, ThisActor == null
+	 *		Unhidghligh LastActor
+	*/
+
+	if(!LastActor)
+	{
+		if(ThisActor)
+		{
+			ThisActor->HighlightActor();//scenario B
+		}
+		//scenario A
+	}
+	else
+	{
+		if(!ThisActor)
+		{
+			LastActor->UnhighlightActor();// scenario E
+		}
+		else
+		{
+			if(LastActor!=ThisActor)
+			{
+				LastActor->UnhighlightActor();
+				ThisActor->HighlightActor();
+			}
+		}
+		
+		
+	}
+	
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -54,3 +110,5 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		
 	}
 }
+
+
